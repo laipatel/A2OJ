18149463610dfc183b6f769b5d3aa230d95eac28
Lai Patel <laipatel@tutanota.com>
2021-12-29 17:00:03 +0530

    update template

--- 800-1299/template.cpp
+++ 800-1299/template.cpp
@@ -27,12 +27,5 @@ int main() {
 	ios_base::sync_with_stdio(false);
   cin.tie(NULL); 
 
-	#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-	#endif
-
-
-
 	return 0;
 }
