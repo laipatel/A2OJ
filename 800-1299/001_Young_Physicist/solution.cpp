7e1e5193685905cd5112db939646f94e7cdc74ad
Lai Patel <laipatel@tutanota.com>
2021-12-29 17:21:55 +0530

    finish young physicist problem

---
+++ 800-1299/001_Young_Physicist/solution.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<string, string> pss;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vpii;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i < b; i++)
+#define SQ(a) (a)*(a)
+
+double eps = 1e-9;
+long long inf = 1000000000000000005;
+int intf = 1000000005;
+double pi = acos(-1);
+
+
+int main() {
+
+	ios_base::sync_with_stdio(false);
+  cin.tie(NULL); 
+
+	int n = 0, x = 0, y = 0, z = 0, val = 0;
+	
+	cin >> n;
+	REP(i, 0, n) {
+		cin >> val;	
+		x+=val;
+		cin >> val;	
+		y+=val;
+		cin >> val;	
+		z+=val;
+	} 
+	if (x == 0 && y == 0 && z == 0) {
+		cout << "YES\n";
+	} else {
+		cout << "NO\n";
+	}
+
+	return 0;
+}
